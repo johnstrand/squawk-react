611a291bf90a3ef455f1efc632412bbbd2823329
John <johdeg@gmail.com>
2018-03-29 20:44:55 +0200

    Updated source based on tslint requirements (mostly)

--- src/Squawk.ts
+++ src/Squawk.ts
@@ -1,62 +1,60 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-declare module 'react' {
+declare module "react" {
     interface Component<P, S> {
+        __squawk_identity: string;
         register<T>(message: string, callback: (value: T) => void): void;
         unregister(): void;
         send(message: string, value: any): void;
-        setSquawk(name: string): void;
-        __squawk__name: string;
+        squawk(name: string): void;
     }
 }
 
-let squawk_registry: { [message: string]: { [subscriber: string]: ((value: any) => void) | undefined } } = {};
-let squawk_history: { [message: string]: any } = {};
+const squawkRegistry: { [message: string]: { [subscriber: string]: ((value: any) => void) | undefined } } = {};
+const squawkHistory: { [message: string]: any } = {};
 
-Component.prototype.setSquawk = function (name: string) {
+Component.prototype.squawk = function (name: string): void {
     this.__squawk__name = name;
-}
+};
 
-Component.prototype.register = function <T>(message: string, callback: (value: T) => void) {
-    if (!squawk_registry[message]) {
-        squawk_registry[message] = {};
+Component.prototype.register = function <T>(message: string, callback: (value: T) => void): void {
+    if (!squawkRegistry[message]) {
+        squawkRegistry[message] = {};
     }
 
     if (!this.__squawk__name) {
-        throw 'setSquawk must be run before a component may register for messages';
+        throw new Error("squawk must be run before a component may register for messages");
     }
 
-    let subscriber = this.__squawk__name;
+    const subscriber: string = this.__squawk__name;
 
-    console.log(`SQUAWK: Registering ${subscriber} for ${message}`);
-    squawk_registry[message][subscriber] = callback;
+    squawkRegistry[message][subscriber] = callback;
 
-    if (squawk_history[message]) {
-        callback(squawk_history[message]);
+    if (squawkHistory[message]) {
+        callback(squawkHistory[message]);
     }
-}
+};
 
-Component.prototype.unregister = function () {
+Component.prototype.unregister = function (): void {
     if (!this.__squawk__name) {
         return;
     }
 
-    let subscriber = this.__squawk__name;
-    console.log(`SQUAWK: Unregistering ${subscriber}`);
-    let messages = Object.getOwnPropertyNames(squawk_registry);
-    messages.forEach(message => squawk_registry[message][subscriber] = undefined);
-}
+    const subscriber: string = this.__squawk__name;
+    const messages: string[] = Object.getOwnPropertyNames(squawkRegistry);
+    messages.forEach((message) => squawkRegistry[message][subscriber] = undefined);
+};
 
-Component.prototype.send = function (message: string, value: any) {
-    console.log(`SQUAWK: Sending ${message} => ${value}`);
-    squawk_history[message] = value;
-    if (!squawk_registry[message]) {
+Component.prototype.send = function (message: string, value: any): void {
+    squawkHistory[message] = value;
+    if (!squawkRegistry[message]) {
         return;
     }
 
-    Object.getOwnPropertyNames(squawk_registry[message]).forEach(subscriber => {
-        let callback = squawk_registry[message][subscriber];
-        if (callback)
-            callback(value)
+    Object.getOwnPropertyNames(squawkRegistry[message]).forEach(subscriber => {
+        let callback: (message: any) => void = squawkRegistry[message][subscriber];
+        if (callback) {
+            callback(value);
+        }
     });
-}
+};
