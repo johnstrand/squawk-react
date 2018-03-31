d8046bb7fdd2aa39cd588566e733c667465e6325
John <johdeg@gmail.com>
2018-03-31 17:03:30 +0200

    Reworked interface signature according to recommendations for overloads

--- src/Squawk.ts
+++ src/Squawk.ts
@@ -4,8 +4,7 @@ declare module "react" {
     interface Component<P, S> {
         __squawk_identity: string;
         register<T>(message: string, callback: (value: T) => void): void;
-        unregister(): void;
-        unregister(message: string): void;
+        unregister(message?: string): void;
         send(message: string, value: any): void;
         squawk(name: string): void;
     }
