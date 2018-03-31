4c3fcaee09e88c85c6a0c43a6ebf550d8153e961
John <johdeg@gmail.com>
2018-03-31 17:15:39 +0200

    Added some JSDoc... hopefully correct too

--- dist/Squawk.js
+++ dist/Squawk.js
@@ -1,15 +1,25 @@
 import { Component } from "react";
 var squawkRegistry = {};
 var squawkHistory = {};
+/**
+ * Sets the name of the component, which is used to track subscriptions
+ * @param {string} name The name of the component
+ */
 Component.prototype.squawk = function (name) {
     this.__squawk__name = name;
 };
+/**
+ * Registers a subscription for a specific message. If Squawk has seen at least one message of that type before, the callback will
+ * be immediately invoked with the last seen message.
+ * @param {string} message The message type
+ * @param {(value: T) => void} callback The callback which will be invoked when a message of previously specified type appears
+ */
 Component.prototype.register = function (message, callback) {
     if (!squawkRegistry[message]) {
         squawkRegistry[message] = {};
     }
     if (!this.__squawk__name) {
-        throw new Error("squawk must be run before a component may register for messages");
+        throw new Error("squawk() must be run before a component may register for messages");
     }
     var subscriber = this.__squawk__name;
     squawkRegistry[message][subscriber] = callback;
@@ -17,14 +27,31 @@ Component.prototype.register = function (message, callback) {
         callback(squawkHistory[message]);
     }
 };
-Component.prototype.unregister = function () {
+/**
+ * Unregisters the component's subscriptions, either a specific one or all of them
+ * @param {string} message Optional parameter specifying which subscription to cancel. If undefined, all subscriptions will be cancelled
+ */
+Component.prototype.unregister = function (message) {
     if (!this.__squawk__name) {
         return;
     }
+    if (message && !squawkRegistry[message]) {
+        return;
+    }
     var subscriber = this.__squawk__name;
-    var messages = Object.getOwnPropertyNames(squawkRegistry);
-    messages.forEach(function (message) { return squawkRegistry[message][subscriber] = undefined; });
+    if (message) {
+        squawkRegistry[message][subscriber] = undefined;
+    }
+    else {
+        var messages = Object.getOwnPropertyNames(squawkRegistry);
+        messages.forEach(function (message) { return squawkRegistry[message][subscriber] = undefined; });
+    }
 };
+/**
+ * Sends a message of the specified type and value
+ * @param {string} message The message type
+ * @param {any} value The message value
+ */
 Component.prototype.send = function (message, value) {
     squawkHistory[message] = value;
     if (!squawkRegistry[message]) {
--- src/Squawk.ts
+++ src/Squawk.ts
@@ -20,10 +20,20 @@ const squawkHistory: {
     [message: string]: any 
 } = {};
 
+/** 
+ * Sets the name of the component, which is used to track subscriptions
+ * @param {string} name The name of the component
+ */
 Component.prototype.squawk = function (name: string): void {
     this.__squawk__name = name;
 };
 
+/**
+ * Registers a subscription for a specific message. If Squawk has seen at least one message of that type before, the callback will
+ * be immediately invoked with the last seen message.
+ * @param {string} message The message type
+ * @param {(value: T) => void} callback The callback which will be invoked when a message of previously specified type appears
+ */
 Component.prototype.register = function <T>(message: string, callback: (value: T) => void): void {
     if (!squawkRegistry[message]) {
         squawkRegistry[message] = {};
@@ -42,6 +52,10 @@ Component.prototype.register = function <T>(message: string, callback: (value: T
     }
 };
 
+/**
+ * Unregisters the component's subscriptions, either a specific one or all of them
+ * @param {string} message Optional parameter specifying which subscription to cancel. If undefined, all subscriptions will be cancelled
+ */
 Component.prototype.unregister = function (message?: string): void {
     if (!this.__squawk__name) {
         return;
@@ -61,6 +75,11 @@ Component.prototype.unregister = function (message?: string): void {
     }
 };
 
+/**
+ * Sends a message of the specified type and value
+ * @param {string} message The message type
+ * @param {any} value The message value
+ */
 Component.prototype.send = function (message: string, value: any): void {
     squawkHistory[message] = value;
     if (!squawkRegistry[message]) {
