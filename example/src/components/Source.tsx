fd24b92c7da500661d4a154e8788d40af553ac85
John Strand <john.strand@dekiru.se>
2018-10-26 20:22:25 +0200

    Actually implemented example

---
+++ example/src/components/Source.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+
+interface ISourceState {
+  text: string;
+}
+
+export class Source extends React.Component<{}, ISourceState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      text: "",
+    };
+  }
+
+  public render() {
+    return (
+      <div>
+        <input
+          value={this.state.text}
+          onChange={(e: React.SyntheticEvent<HTMLInputElement>) =>
+            this.setState({ text: e.currentTarget.value })
+          }
+        />
+        <button onClick={this.sendMessage}>Send</button>
+      </div>
+    );
+  }
+
+  private sendMessage = () => {
+    this.send("add_message", (messages: string[]) => {
+        console.log(messages);
+      if (!messages) {
+        messages = [];
+      }
+      messages.push(this.state.text);
+      return messages;
+    });
+
+    this.setState({ text: "" });
+  }
+}
