09061092b5bcb76975581b642ee8ac861e37d8f5
John Strand <john.strand@dekiru.se>
2018-11-28 17:55:16 +0100

    Implemented helpful wrapping method that maybe works

--- src/Squawk.tsx
+++ src/Squawk.tsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import * as React from "react";
 
 declare module "react" {
     interface Component<P, S> {
@@ -6,7 +7,11 @@ declare module "react" {
         clear(message: string): void;
         getMessage<T>(message: string): T | undefined;
         getRegistrations(): string[];
-        register<T>(message: string, callback: (value: T) => void, ignoreLast?: boolean): void;
+        register<T>(
+            message: string,
+            callback: (value: T) => void,
+            ignoreLast?: boolean
+        ): void;
         send(message: string, value: any): void;
         squawk(name: string): void;
         unregister(message?: string): void;
@@ -20,65 +25,69 @@ declare module "react" {
      */
     const squawkRegistry: {
         [message: string]: {
-            [subscriber: string]: ((value: any) => void) | undefined
-        }
+            [subscriber: string]: ((value: any) => void) | undefined;
+        };
     } = {};
 
     /**
      * Contains the last payload of each message type
      */
     const squawkHistory: {
-        [message: string]: any
+        [message: string]: any;
     } = {};
 
     let loggingEnabled: boolean = false;
 
     const log: (text: string) => void = (text: string) => {
-        if(!loggingEnabled) {
+        if (!loggingEnabled) {
             return;
         }
         console.log(log);
-    }
+    };
 
     /**
      * Sets whether or not logging is enabled
      * @param {boolean} enabled Sets logging enabled or disabled
      */
-    Component.prototype.setLogging = function (enabled: boolean): void {
+    Component.prototype.setLogging = function(enabled: boolean): void {
         loggingEnabled = enabled;
-        log('Logging enabled');
-    }
+        log("Logging enabled");
+    };
 
     /**
      * Removes the last seen message of the specified type
      * @param {string} message Message type
      */
-    Component.prototype.clear = function (message: string): void {
+    Component.prototype.clear = function(message: string): void {
         log(`Cleared messages of type ${message}`);
         squawkHistory[message] = undefined;
-    }
+    };
 
     /**
      * Retrieves the last seen message of the specified type
      * @param {string} message Message type
      * @returns {T | undefined} A message of type T (or undefined, if the message has not been previously seen)
      */
-    Component.prototype.getMessage = function <T>(message: string): T | undefined {
+    Component.prototype.getMessage = function<T>(
+        message: string
+    ): T | undefined {
         if (squawkHistory[message]) {
             return squawkHistory[message];
         } else {
             return undefined;
         }
-    }
+    };
 
     /**
      * Retrieves a list of message types that the current component is subscribed to
      * @returns {string[]} A list of message types
      */
-    Component.prototype.getRegistrations = function (): string[] {
+    Component.prototype.getRegistrations = function(): string[] {
         const subscriber: string = this.__squawk_identity;
-        return Object.getOwnPropertyNames(squawkRegistry).filter(message => !!squawkRegistry[message][subscriber]);
-    }
+        return Object.getOwnPropertyNames(squawkRegistry).filter(
+            message => !!squawkRegistry[message][subscriber]
+        );
+    };
 
     /**
      * Registers a subscription for a specific message. If Squawk has seen at least one message of that type before, the callback will
@@ -87,14 +96,20 @@ declare module "react" {
      * @param {(value: T) => void} callback The callback which will be invoked when a message of previously specified type appears
      * @param {boolean} ignoreLast Indicates that the callback should not be immediately called even if there exists a previous message of the specified type
      */
-    Component.prototype.register = function <T>(message: string, callback: (value: T) => void, skipLast?: boolean): void {
+    Component.prototype.register = function<T>(
+        message: string,
+        callback: (value: T) => void,
+        skipLast?: boolean
+    ): void {
         log(`Registering subscriber ${this.__squawk_identity} for ${message}`);
         if (!squawkRegistry[message]) {
             squawkRegistry[message] = {};
         }
 
         if (!this.__squawk_identity) {
-            throw new Error("squawk() must be run before a component may register for messages");
+            throw new Error(
+                "squawk() must be run before a component may register for messages"
+            );
         }
 
         const subscriber: string = this.__squawk_identity;
@@ -112,26 +127,29 @@ declare module "react" {
      * @param {string} message The message type
      * @param {any} value The message value
      */
-    Component.prototype.send = function (message: string, value: any): void {
+    Component.prototype.send = function(message: string, value: any): void {
         log(`${this.__squawk_identity} sending ${message}`);
         squawkHistory[message] = value;
         if (!squawkRegistry[message]) {
             return;
         }
 
-        Object.getOwnPropertyNames(squawkRegistry[message]).forEach(subscriber => {
-            let callback: ((message: any) => void) | undefined = squawkRegistry[message][subscriber];
-            if (callback) {
-                callback(value);
+        Object.getOwnPropertyNames(squawkRegistry[message]).forEach(
+            subscriber => {
+                let callback: ((message: any) => void) | undefined =
+                    squawkRegistry[message][subscriber];
+                if (callback) {
+                    callback(value);
+                }
             }
-        });
+        );
     };
 
-    /** 
+    /**
      * Sets the name of the component, which is used to track subscriptions
      * @param {string} name The name of the component
      */
-    Component.prototype.squawk = function (name: string): void {
+    Component.prototype.squawk = function(name: string): void {
         this.__squawk_identity = name;
     };
 
@@ -139,7 +157,7 @@ declare module "react" {
      * Unregisters the component's subscriptions, either a specific one or all of them
      * @param {string} message Optional parameter specifying which subscription to cancel. If undefined, all subscriptions will be cancelled
      */
-    Component.prototype.unregister = function (message?: string): void {
+    Component.prototype.unregister = function(message?: string): void {
         if (!this.__squawk_identity) {
             return;
         }
@@ -153,8 +171,33 @@ declare module "react" {
         if (message) {
             squawkRegistry[message][subscriber] = undefined;
         } else {
-            const messages: string[] = Object.getOwnPropertyNames(squawkRegistry);
-            messages.forEach((message) => squawkRegistry[message][subscriber] = undefined);
+            const messages: string[] = Object.getOwnPropertyNames(
+                squawkRegistry
+            );
+            messages.forEach(
+                message => (squawkRegistry[message][subscriber] = undefined)
+            );
         }
     };
 })();
+
+export function squawk(Component: React.ComponentType): React.ComponentType {
+    const generateName = (): string =>
+        Math.random()
+            .toString(36)
+            .substring(7);
+
+    return class extends React.Component {
+        constructor(props: any) {
+            super(props);
+            (Component as any).prototype.squawk(generateName());
+        }
+        render(): JSX.Element {
+            return <Component />;
+        }
+
+        componentWillUnmount() {
+            (Component as any).prototype.unregister();
+        }
+    };
+}
