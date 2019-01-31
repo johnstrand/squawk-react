2057e9cc6ab959a6cd80b70f57e7e2f0f5c4d24a
John Strand <john.strand@dekiru.se>
2019-01-31 21:11:55 +0100

    Updated build

--- dist/Squawk.js
+++ dist/Squawk.js
@@ -11,6 +11,17 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 import * as React from "react";
 var RootTracker = (function () {
     var squawkRegistry = {};
@@ -73,7 +84,7 @@ function squawk(componentTypeConstructor) {
             return _super !== null && _super.apply(this, arguments) || this;
         }
         class_1.prototype.render = function () {
-            return React.createElement(ConstructedType, null);
+            return React.createElement(ConstructedType, __assign({}, this.props));
         };
         class_1.prototype.componentWillUnmount = function () {
             RootTracker.unregister(name);
