64e69fd359029e2b1e2bd28ab8fb2bb1127c2b27
John Strand <john.strand@dekiru.se>
2019-02-05 20:00:28 +0100

    Ran linting

--- src/Squawk.tsx
+++ src/Squawk.tsx
@@ -7,17 +7,22 @@ export function createStore<IStore>(initalState: IStore) {
         Math.random()
             .toString(36)
             .substring(7);
-    
+
     const state = initalState;
-    const callbacks: IDictionary<IDictionary<(value: any) => void>> & object = {};
+    const callbacks: IDictionary<IDictionary<(value: any) => void>> &
+        object = {};
 
     type StoreKey = keyof IStore;
     type Reducer<T extends StoreKey> = (value: IStore[T]) => IStore[T];
     type Callback<T extends StoreKey> = (value: IStore[T]) => any;
 
     const get = <T extends StoreKey>(event: T) => state[event];
-    const subscribe = <T extends StoreKey>(event: T, subscriber: string, callback: Callback<T>) => {
-        if(!callbacks.hasOwnProperty(event)) {
+    const subscribe = <T extends StoreKey>(
+        event: T,
+        subscriber: string,
+        callback: Callback<T>
+    ) => {
+        if (!callbacks.hasOwnProperty(event)) {
             callbacks[event as string] = {};
         }
         callbacks[event as string][subscriber] = callback;
@@ -27,15 +32,20 @@ export function createStore<IStore>(initalState: IStore) {
             delete callbacks[event][subscriber];
         });
     };
-    const update = <T extends StoreKey>(event: T, reducer: Reducer<T>): void => {
+    const update = <T extends StoreKey>(
+        event: T,
+        reducer: Reducer<T>
+    ): void => {
         const newValue = reducer(state[event]);
         state[event] = newValue;
-        if(!callbacks.hasOwnProperty(event)) {
+        if (!callbacks.hasOwnProperty(event)) {
             return;
         }
-        Object.getOwnPropertyNames(callbacks[event as string]).forEach(subscriber => {
-            callbacks[event as string][subscriber](newValue);
-        });
+        Object.getOwnPropertyNames(callbacks[event as string]).forEach(
+            subscriber => {
+                callbacks[event as string][subscriber](newValue);
+            }
+        );
     };
 
     return {
@@ -48,15 +58,25 @@ export function createStore<IStore>(initalState: IStore) {
         },
         unsubscribe,
         squawk<P>(
-            componentTypeConstructor: (subscribe: <T extends StoreKey>(event: T, callback: (value: IStore[T]) => any) => void) => React.ComponentType<P>
+            componentTypeConstructor: (
+                subscribe: <T extends StoreKey>(
+                    event: T,
+                    callback: (value: IStore[T]) => any
+                ) => void
+            ) => React.ComponentType<P>
         ): React.ComponentType<P> {
             const name = generateName();
 
-            const componentSubscribe = <T extends StoreKey>(event: T, callback: (value: IStore[T]) => any) => {
+            const componentSubscribe = <T extends StoreKey>(
+                event: T,
+                callback: (value: IStore[T]) => any
+            ) => {
                 subscribe(event, name, callback);
             };
 
-            const ConstructedType = componentTypeConstructor(componentSubscribe);
+            const ConstructedType = componentTypeConstructor(
+                componentSubscribe
+            );
 
             return class extends React.Component<P> {
                 render() {
