8bced65c429d5a3cede6f73134230543d4fd89c0
John Strand <john.strand@dekiru.se>
2019-04-29 21:58:51 +0200

    Massive rework of everything

--- src/Squawk.tsx
+++ src/Squawk.tsx
@@ -6,74 +6,59 @@ type NotNever<T> = Pick<
     ({ [P in keyof T]: T[P] extends never ? never : P })[keyof T]
 >;
 
-type FunctionComponentHandler = {
-    callback: () => void;
-    events: Set<string>;
-};
-
-type FunctionComponentTracker = CallbackTracker<FunctionComponentHandler>;
-
-type CallbackTracker<T> = Map<string, T>;
-
-type ClassComponentTracker = Map<string, CallbackTracker<(value: any) => void>>;
-
 export function createStore<IStore>(initialState: NotNever<IStore>) {
-    const generateName = (): string =>
+    const generateId = (): string =>
         Math.random()
             .toString(36)
             .substring(7);
 
-    // Set initial state, could possibly use JSON.stringify/parse to break references
-    const state = initialState as IStore;
-    // The callback structure is callbacks[event][subscriber]
-    const classComponentCallbacks: ClassComponentTracker = new Map();
-    const functionComponentCallbacks: FunctionComponentTracker = new Map();
-
     // Helper types to make things less verbose
     type StoreKey = keyof IStore;
     type Reducer<T extends StoreKey> = (value: IStore[T]) => IStore[T];
-    type Callback<T extends StoreKey> = (value: IStore[T]) => any;
+    type TrackedComponent = { isFunction: boolean };
 
-    // Simply returns the value of specified event
+    // Set initial state, could possibly use JSON.stringify/parse to break references
+    const state = initialState as IStore;
+
+    // Tracker for function components, structure is [event][components]
+    const functionComponentTracker: Map<
+        string,
+        Set<React.Component>
+    > = new Map();
+
+    // Tracker for class components, structure is [component][events[]]
+    const classComponentTracker: Map<
+        React.Component,
+        (() => void)[]
+    > = new Map();
+
+    // Tracker for subscriptions, structure is [event][ID][callback]
+    const subscriptions: Map<
+        string,
+        Map<string, (value: any) => any>
+    > = new Map();
+
+    // Returns the value of specified event
+    // If called from with a rendering function component, will also set up a subscription
     const get = <T extends StoreKey>(event: T) => {
-        if (renderingFunctionalComponent) {
-            const componentName = functionalComponentContexts.peek();
-            (functionComponentCallbacks.get(
-                componentName
-            ) as FunctionComponentHandler).events.add(event as string);
+        if (isRendering) {
+            const activeComponent = components.peek() as React.Component &
+                TrackedComponent;
+
+            if (activeComponent.isFunction) {
+                let set = functionComponentTracker.get(event as string);
+                if (!set) {
+                    set = new Set<React.Component>();
+                }
+
+                functionComponentTracker.set(
+                    event as string,
+                    set.add(activeComponent)
+                );
+            }
         }
+
         return state[event];
-    };
-
-    // Subscribe helper method
-    const subscribe = <T extends StoreKey>(
-        event: T,
-        subscriber: string,
-        callback: Callback<T>
-    ) => {
-        // Have we seen this event before? If not, add it
-        if (!classComponentCallbacks.has(event as string)) {
-            classComponentCallbacks.set(event as string, new Map());
-        }
-
-        // Register the event callback for the subscriber
-        (classComponentCallbacks.get(event as string) as Map<string, any>).set(
-            subscriber,
-            callback
-        );
-    };
-
-    const unsubscribe = <T extends StoreKey>(subscriber: string, event?: T) => {
-        if (event) {
-            //delete classComponentCallbacks[event as string][subscriber];
-            (classComponentCallbacks.get(event as string) as Map<
-                string,
-                any
-            >).delete(subscriber);
-            return;
-        }
-
-        classComponentCallbacks.forEach(k => k.delete(subscriber));
     };
 
     function update<T extends StoreKey>(
@@ -93,132 +78,133 @@ export function createStore<IStore>(initialState: NotNever<IStore>) {
 
         state[event] = newValue as IStore[T];
 
-        functionComponentCallbacks.forEach(subscriber => {
-            if (subscriber.events.has(event as string)) {
-                subscriber.callback();
-            }
-        });
+        const trackedFunctionComponents = functionComponentTracker.get(
+            event as string
+        );
 
-        // If no one is listening, exit the method here
-        if (!classComponentCallbacks.has(event as string)) {
-            return;
+        if (trackedFunctionComponents) {
+            trackedFunctionComponents.forEach(component =>
+                component.forceUpdate()
+            );
         }
 
-        // Loop through every subscriber registered for the event and
-        // invoke the their callbacks with the new value
+        const subscriptionTrackers = subscriptions.get(event as string);
 
-        (classComponentCallbacks.get(event as string) as Map<
-            string,
-            Callback<T>
-        >).forEach(callback => {
-            callback(newValue as IStore[T]);
-        });
+        if (subscriptionTrackers) {
+            subscriptionTrackers.forEach(callback => callback(newValue));
+        }
     }
 
-    const classComponentContexts: Stack<string> = new Stack();
-    const functionalComponentContexts: Stack<string> = new Stack();
-    let renderingFunctionalComponent: boolean = false;
-    let inConstructor: boolean = false;
+    const components: Stack<React.Component> = new Stack();
+    let isRendering: number = 0;
+
+    // extends any is there to stop the transpiler from thinking that <T> is an element
+    const renderScope = <T extends any>($this: any, scope: () => T) => {
+        isRendering++;
+        components.push($this);
+        const rendered = scope();
+        components.pop();
+        isRendering--;
+        return rendered;
+    };
 
     return {
         get,
         update,
-        subscribe<T extends StoreKey>(event: T, callback: Callback<T>) {
-            if (inConstructor) {
-                throw Error(
-                    "Do not use subscribe() in constructor, use componentDidMount()"
-                );
-            }
-            // If any ambient context exists, use that, otherwise generate a random name to use for setting up the subscription
-            const name = classComponentContexts.any()
-                ? classComponentContexts.peek()
-                : generateName();
-            subscribe(event, name, callback);
-            return () => unsubscribe(name, event);
-        },
-        createBinder(
-            ref: React.Component,
-            subscriber: <T extends StoreKey>(
-                event: T,
-                callback: (value: IStore[T]) => any
-            ) => any
+        subscribe<T extends StoreKey>(
+            event: T,
+            callback: (value: IStore[T]) => any
         ) {
-            // Return a function that captures the component reference and subscriber callback
-            // and simplifies subscribing to an event with the same name as a local event
-            return <T extends StoreKey>(...events: T[]) => {
-                events.forEach(event =>
-                    subscriber(event, value => ref.setState({ [event]: value }))
+            if (isRendering) {
+                throw Error("Do not call subscribe inside a rendering call");
+            }
+            const id = generateId();
+
+            const unsubscribe = () => {
+                const eventSubscribersToRemove = subscriptions.get(
+                    event as string
                 );
+                if (!eventSubscribersToRemove) {
+                    return;
+                }
+
+                eventSubscribersToRemove.delete(id);
             };
+
+            if (components.any()) {
+                const currentComponent = components.peek() as React.Component &
+                    TrackedComponent;
+                if (currentComponent.isFunction) {
+                    throw Error(
+                        "Do not call subscribe from a function component"
+                    );
+                }
+
+                classComponentTracker.set(currentComponent, [
+                    ...(classComponentTracker.get(currentComponent) || []),
+                    unsubscribe
+                ]);
+            }
+
+            const eventSubscribers =
+                subscriptions.get(event as string) ||
+                new Map<string, (value: any) => any>();
+
+            subscriptions.set(
+                event as string,
+                eventSubscribers.set(id, callback)
+            );
+
+            return unsubscribe;
         },
         squawk<P>(
-            SquawkComponent: React.ComponentClass<P>
-        ): React.ComponentType<P> {
-            return class extends SquawkComponent {
-                private name: string = generateName();
-
-                constructor(props: P) {
-                    // Ugly hack, or ugliest hack?
-                    // This makes sure that the current context is set
-                    // before invoking the parent constructor
-                    super(
-                        (() => {
-                            inConstructor = true;
-                            renderingFunctionalComponent = false;
-                            return props;
-                        })()
-                    );
-                    this.name = generateName();
-                    inConstructor = false;
-                }
-
-                render() {
-                    renderingFunctionalComponent = false;
-                    return super.render();
-                }
-
-                componentDidMount() {
-                    if (!super.componentDidMount) {
-                        return;
+            ComponentType: React.ComponentType<P>
+        ): React.ComponentClass<P> {
+            if (ComponentType.prototype.isReactComponent) {
+                return class extends (ComponentType as React.ComponentClass<
+                    P
+                >) {
+                    public isFunction = false;
+                    render() {
+                        return renderScope(this, () => super.render());
                     }
-                    renderingFunctionalComponent = false;
-                    classComponentContexts.push(this.name);
-                    super.componentDidMount();
-                    classComponentContexts.pop();
-                }
 
-                componentWillUnmount() {
-                    if (super.componentWillUnmount) {
-                        super.componentWillUnmount();
+                    componentDidMount() {
+                        if (!super.componentDidMount) {
+                            return;
+                        }
+                        components.push(this);
+                        super.componentDidMount();
+                        components.pop();
                     }
-                    unsubscribe(this.name);
-                }
-            };
-        },
-        connect<P>(Component: React.FunctionComponent<P>) {
-            return class extends React.Component<P, P> {
-                public name: string = generateName();
 
-                constructor(props: P) {
-                    super(props);
-                    functionComponentCallbacks.set(this.name, {
-                        callback: () => this.forceUpdate(),
-                        events: new Set()
-                    });
-                }
+                    componentWillUnmount() {
+                        const unsubscribers = classComponentTracker.get(this);
+                        if (!unsubscribers) {
+                            return;
+                        }
 
-                render() {
-                    renderingFunctionalComponent = true;
-                    functionalComponentContexts.push(this.name);
-                    const renderedComponent = Component(this.props);
-                    functionalComponentContexts.pop();
-                    return renderedComponent;
-                }
+                        unsubscribers.forEach(unsubscribe => unsubscribe());
+                    }
+                };
+            } else {
+                return class extends React.Component<P> {
+                    public isFunction = true;
+                    render() {
+                        return renderScope(this, () =>
+                            (ComponentType as React.FunctionComponent<P>)(
+                                this.props
+                            )
+                        );
+                    }
 
-                componentWillUnmount() {
-                    functionComponentCallbacks.delete(this.name);
-                }
-            };
+                    componentWillUnmount() {
+                        functionComponentTracker.forEach(list =>
+                            list.delete(this)
+                        );
+                    }
+                };
+            }
         }
     };
 }
