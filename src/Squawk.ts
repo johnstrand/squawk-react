f1dcbad82301292d6d9f38ccd698f84d0f928f23
John Strand <john.strand@dekiru.se>
2019-05-14 21:10:42 +0200

    Reworked to use useReducer

--- src/Squawk.ts
+++ src/Squawk.ts
@@ -1,120 +1,87 @@
-import { useEffect, useState } from "react";
+import { useReducer, useEffect } from "react";
 
-const createStore = <IStore>(initial: IStore) => {
+export default function createStore<TStore>(initial: TStore) {
+    type StoreKey = keyof TStore;
     const getId = () => Math.random().toString(36);
 
-    /** Type alias for the props in the store */
-    type StoreKey = keyof IStore;
-    /** Utility type for reducer functions */
-    type Reducer<T extends StoreKey> = (value: IStore[T]) => IStore[T];
+    const subscribers = new Map<string, Set<string>>();
+    const subscriberCallbacks = new Map<string, (value: any) => any>();
 
-    /** Map used to track props->id->callback */
-    const subscribers: Map<
-        StoreKey,
-        Map<string, (value: any) => any>
-    > = new Map();
+    const internalUpdate = (value: any) => {
+        const contexts = Object.getOwnPropertyNames(value);
+        const ids = contexts
+            .filter(context => subscribers.has(context))
+            .map(context => Array.from(subscribers.get(context)!))
+            .reduce((acc, cur) => [...acc, ...cur]);
 
-    /** Internal subscribe method */
-    const internalSubscribe = <TContext extends StoreKey>(
-        context: TContext,
-        callback: (value: IStore[TContext]) => any
+        initial = { ...initial, ...value };
+
+        ids.forEach(id => {
+            if (!subscriberCallbacks.has(id)) {
+                return;
+            }
+
+            subscriberCallbacks.get(id)!(initial);
+        });
+    };
+
+    const internalSubscribe = (
+        contexts: string[],
+        reducer: (state: any) => any
     ) => {
-        /** Get or create a map of subscribers for the current context */
-        const contextSubscribers =
-            subscribers.get(context) || new Map<string, (value: any) => any>();
         const id = getId();
-        contextSubscribers.set(id, callback);
-        subscribers.set(context, contextSubscribers);
+        contexts.forEach(context => {
+            if (!subscribers.has(context)) {
+                subscribers.set(context, new Set<string>());
+            }
+            subscribers.get(context)!.add(id);
+        });
+
+        subscriberCallbacks.set(id, reducer);
+
         return () => {
-            subscribers.get(context)!.delete(id);
+            subscribers.forEach(s => s.delete(id));
+            subscriberCallbacks.delete(id);
         };
     };
 
-    /** Internal update method */
-    const internalUpdate = <TContext extends StoreKey>(
-        context: TContext,
-        value: any
-    ) => {
-        // If value is undefined, or has changed, proceed with the update
-        if(value !== undefined && value === initial[context]) {
-            return;
-        }
-
-        initial[context] = value;
-        
-        const contextSubscribers = subscribers.get(context);
-        if (contextSubscribers) {
-            contextSubscribers.forEach(callback => callback(value));
-        }
-    };
-
-    /** Update multiple values */
-    function update<TContext extends StoreKey>(
-        reducer: (store: IStore) => Pick<IStore, TContext>
-    ): void;
-    /** Direct update (without reducer) */
-    function update<TContext extends StoreKey>(
-        context: TContext,
-        value: IStore[TContext]
-    ): void;
-    /** Update via reducer */
-    function update<TContext extends StoreKey>(
-        context: TContext,
-        reducer: Reducer<TContext>
-    ): void;
-    /** Implementation */
-    function update<TContext extends StoreKey>(
-        contextOrReducer: any,
-        reducerOrValue?: any
-    ): void {
-        if (typeof contextOrReducer === "function") {
-            const newValues = (contextOrReducer as (
-                store: IStore
-            ) => Pick<IStore, TContext>)(initial);
-
-            Object.getOwnPropertyNames(newValues).forEach(prop => {
-                internalUpdate(prop as StoreKey, newValues[prop as TContext]);
-            });
-        } else {
-            const newValue =
-                typeof reducerOrValue === "function"
-                    ? reducerOrValue(initial[contextOrReducer as TContext])
-                    : reducerOrValue;
-
-            internalUpdate(contextOrReducer, newValue);
-        }
-    }
-
     return {
-        update,
-        get<TContext extends StoreKey>(context: TContext): IStore[TContext] {
+        get<T extends StoreKey>(context: T): TStore[T] {
             return initial[context];
         },
-        subscribe<TContext extends StoreKey>(
-            context: TContext,
-            callback: (value: IStore[TContext]) => any
+        subscribe<T extends StoreKey>(
+            context: T,
+            callback: (value: TStore[T]) => any
         ): () => void {
-            return internalSubscribe(context, callback);
+            return internalSubscribe([context as string], (state: TStore) =>
+                callback(state[context])
+            );
         },
-        useSquawk<TContext extends StoreKey>(
-            context: TContext
-        ): [IStore[TContext], (value: IStore[TContext]) => any] {
-            const [stateValue, setStateValue] = useState<IStore[TContext]>(
-                initial[context]
-            );
+        update<T extends StoreKey>(
+            reducer: (value: TStore) => Pick<TStore, T>
+        ): void {
+            internalUpdate(reducer(initial));
+        },
+        useSquawk<T extends StoreKey>(
+            localReducer: (value: TStore) => Pick<TStore, T>
+        ): [Pick<TStore, T>, (state: Pick<TStore, T>) => void] {
+            const reducer = (state: any, action: any) => ({
+                ...state,
+                ...action
+            });
 
-            /** Setup useEffect to handle unsubscribe when the component unmounts */
+            const localValue = localReducer(initial);
+
+            const [value, dispatcher] = useReducer(reducer, localValue);
+
             useEffect(() =>
-                internalSubscribe(context, value => setStateValue(value))
+                internalSubscribe(
+                    Object.getOwnPropertyNames(localValue),
+                    value => dispatcher(localReducer(value))
+                )
             );
 
-            /** Return value and update function */
-            return [
-                stateValue as IStore[TContext],
-                (value: IStore[TContext]) => update(context, value)
-            ];
+            return [value, (state: Pick<TStore, T>) => internalUpdate(state)];
         }
     };
-};
-
-export default createStore;
+}
