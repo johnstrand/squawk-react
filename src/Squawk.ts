a79b2169d4b3dcfbe3950149955e3cc1eeec396f
John Strand <john.strand@dekiru.se>
2019-05-16 21:38:12 +0200

    Reworked to use function identity

--- src/Squawk.ts
+++ src/Squawk.ts
@@ -9,20 +9,15 @@ export default function createStore<TStore>(globalState: TStore) {
         V
     >;
 
-    /** Helper method to generate random string */
-    const getId = () => Math.random().toString(36);
-
     /** Helper method to produce unique union */
-    const union = (...arrays: string[][]) => {
-        const items = new Set<string>();
+    const union = <T>(...arrays: T[][]) => {
+        const items = new Set<T>();
         arrays.forEach(array => array.forEach(item => items.add(item)));
         return Array.from(items);
     };
 
     /** Map that links individual keys in TStore to the subscriber IDs */
-    const subscribers = new Map<string, Set<string>>();
-    /** Map that links subscriber IDs to their respective update methods */
-    const subscriberCallbacks = new Map<string, (value: any) => any>();
+    const subscribers = new Map<string, Set<(state: any) => any>>();
 
     /** Actual update method, handles resolving subscribers and IDs */
     const internalUpdate = (value: any) => {
@@ -33,7 +28,7 @@ export default function createStore<TStore>(globalState: TStore) {
         /** Get a list of affected contexts from value object */
         const contexts = Object.keys(value);
         /** Get a (non-unique) list of affected ids */
-        const ids = contexts
+        const reducers = contexts
             .filter(context => subscribers.has(context))
             .map(context => Array.from(subscribers.get(context)!));
 
@@ -41,11 +36,8 @@ export default function createStore<TStore>(globalState: TStore) {
         globalState = { ...globalState, ...value };
 
         /** Get unique ids and invoke callbacks for each */
-        union(...ids).forEach(id => {
-            if (!subscriberCallbacks.has(id)) {
-                return;
-            }
-            subscriberCallbacks.get(id)!(globalState);
+        union(...reducers).forEach(reducer => {
+            reducer(globalState);
         });
     };
 
@@ -53,21 +45,17 @@ export default function createStore<TStore>(globalState: TStore) {
         contexts: string[],
         reducer: (state: any) => any
     ) => {
-        const id = getId();
         /** For each supplied context, set up a context->id mapping (and associated callback) */
         contexts.forEach(context => {
             if (!subscribers.has(context)) {
-                subscribers.set(context, new Set<string>());
+                subscribers.set(context, new Set());
             }
-            subscribers.get(context)!.add(id);
+            subscribers.get(context)!.add(reducer);
         });
-
-        subscriberCallbacks.set(id, reducer);
 
         /** Return a function that can be used to remove subscriptions */
         return () => {
-            subscribers.forEach(s => s.delete(id));
-            subscriberCallbacks.delete(id);
+            subscribers.forEach(s => s.delete(reducer));
         };
     };
 
