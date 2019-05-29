1b0b2dca8a121d34cacb3eeec9abf04fab3cb07d
John Strand <john.strand@dekiru.se>
2019-05-29 15:51:23 +0200

    Updates

---
+++ precache-manifest.431a933376fc6cc260b63557218fc8dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b35e945a4df78e53b28f17275d3ca4",
+    "url": "/squawk-react/index.html"
+  },
+  {
+    "revision": "c5cfe989674b6a8f58b3",
+    "url": "/squawk-react/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/squawk-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c5cfe989674b6a8f58b3",
+    "url": "/squawk-react/static/js/main.cd8340dc.chunk.js"
+  },
+  {
+    "revision": "051e2c1976accf93f1d9",
+    "url": "/squawk-react/static/js/runtime~main.c49a8709.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/squawk-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
