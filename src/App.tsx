040197efc32ec219b37a4d048c96aec8072f57db
John Strand <john.strand@dekiru.se>
2019-05-29 16:04:38 +0200

    Tiny tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This app is a work in progress
         </p>
         <a
           className="App-link"
